3afe1903d3aa47712485634e32ce293df2d3fc07
VNiemi <ville.niemi@sykemediapaja.com>
2017-10-01 00:23:28 +0300

    Added class and objects for defining routes. Does not actually do anything yet as the spawner and movement code do not exist yet.

---
+++ Deep Sea/Assets/Scripts/Route.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * This class is a component to add to a level that defines a movement route
+ * for the attackers to follow. It is basically a UI convenience that allows us
+ * to make routes in the editor and to define several routes per level if necessary.
+ */
+public class Route : MonoBehaviour {
+
+	[SerializeField, Tooltip("Waypoints of the route. The first is the spawn point and the last should be the base.")]
+	private GameObject[] _waypoints;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
