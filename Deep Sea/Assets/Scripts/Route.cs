52c32569f806619304d1bfea2df03c7e0f5416d9
VNiemi <ville.niemi@sykemediapaja.com>
2017-10-01 00:52:19 +0300

    Added wave class. Had to redo the route while doing it. Should be easier to use now. You can add routes using the RoutePrefab, define the waypoints, and then use the route to define waves.

--- Deep Sea/Assets/Scripts/Route.cs
+++ Deep Sea/Assets/Scripts/Route.cs
@@ -2,11 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/*
- * This class is a component to add to a level that defines a movement route
- * for the attackers to follow. It is basically a UI convenience that allows us
- * to make routes in the editor and to define several routes per level if necessary.
- */
 public class Route : MonoBehaviour {
 
 	[SerializeField, Tooltip("Waypoints of the route. The first is the spawn point and the last should be the base.")]
---
+++ Deep Sea/Assets/Scripts/Wave.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour {
+
+	[SerializeField, Tooltip("Time in seconds from scene start to wave start.")]
+	private float _waveStart;
+
+	[SerializeField, Tooltip("Time in seconds between enemies in the wave.")]
+	private float _spawnInterval;
+
+	[SerializeField, Tooltip("A previously defined route. You need to create this as a GameObject in the scene. Use the prefab.")]
+	private Route _route;
+
+	[SerializeField, Tooltip("The prefabs to use as enemies, in order.")]
+	private GameObject[] _enemies;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
