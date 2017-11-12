cbd6fa9c3e56c3117b4b57694824554859affcfd
VNiemi <ville.niemi@sykemediapaja.com>
2017-11-12 10:11:17 +0200

    Graphical build menu. The available assets look "correct" and build menu still works the same as before. Visual cues for option being enabled or disabled do NOT work as I do not have the correct assets for all the options in both states. I could have used what is available as placeholders, but there is no point as the only options that actually do this are the two that I don't have assets for...

--- Deep Sea/Assets/Scripts/BuildMenu.cs
+++ Deep Sea/Assets/Scripts/BuildMenu.cs
@@ -59,12 +59,12 @@ public class BuildMenu : MonoBehaviour {
 
         if (tile.OnLeft)
         {
-            gameObject.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Right, 50, 240);
+            gameObject.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Right, 50, 260);
             Debug.Log("Menu open on left.");
         }
         else
         {
-            gameObject.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 50, 240);
+            gameObject.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 50, 260);
             Debug.Log("Menu open on right.");
         }
 
