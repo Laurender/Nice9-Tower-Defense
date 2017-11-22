36262dda63be08ae1626c83030e001e3e737f497
Lauri Johannes Leponiemi <lauri.leponiemi@cs.tamk.fi>
2017-11-22 20:31:27 +0200

    Tried to make a material that could change enemy color based on damage taken, but you couldn't make the enemy red with it. I'll leave it in incase we find another use for it. Also, new scene we used in a playtest.

--- Deep Sea/Assets/Scripts/SceneChanger.cs
+++ Deep Sea/Assets/Scripts/SceneChanger.cs
@@ -14,13 +14,13 @@ public class SceneChanger : MonoBehaviour {
 	void Update () {
 		if (Input.GetKey (KeyCode.Alpha1)) {
 			SceneManager.LoadScene (0);
-		} else if (Input.GetKey (KeyCode.Alpha2)) {
+		}/* else if (Input.GetKey (KeyCode.Alpha2)) {
 			SceneManager.LoadScene (1);
 		} else if (Input.GetKey (KeyCode.Alpha3)) {
 			SceneManager.LoadScene (2);
 		} else if (Input.GetKey (KeyCode.Alpha4)) {
 			SceneManager.LoadScene (3);
-		} else if (Input.GetKey (KeyCode.Escape)) {
+		}*/ else if (Input.GetKey (KeyCode.Escape)) {
 			Application.Quit ();
 		}
 	}
