a79fd72d81498d70b3f2f9dbb819fb3ee17618b0
VNiemi <ville.niemi@cs.tamk.fi>
2017-12-14 15:14:59 +0200

    Music and sound on/off after several dumb copy paste errors. Wiki cleanup.

---
+++ Deep Sea/Assets/Scripts/AudioControls.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioControls : MonoBehaviour
+{
+
+    [SerializeField]
+    UnityEngine.UI.Image _soundImage;
+
+    [SerializeField]
+    Sprite _soundEnabled;
+
+    [SerializeField]
+    Sprite _soundDisabled;
+
+    [SerializeField]
+    UnityEngine.UI.Image _musicImage;
+
+    [SerializeField]
+    Sprite _musicEnabled;
+
+    [SerializeField]
+    Sprite _musicDisabled;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        _soundImage.sprite = MusicController.PlaySFX ? _soundEnabled : _soundDisabled;
+        _musicImage.sprite = MusicController.PlayMusic ? _musicEnabled : _musicDisabled;
+
+    }
+
+    public void MusicButton()
+    {
+        MusicController.PlayMusic = MusicController.PlayMusic ? false : true;
+        _musicImage.sprite = MusicController.PlayMusic ? _musicEnabled : _musicDisabled;
+    }
+
+    public void SoundButton()
+    {
+        MusicController.PlaySFX = MusicController.PlaySFX ? false : true;
+        _soundImage.sprite = MusicController.PlaySFX ? _soundEnabled : _soundDisabled;
+    }
+
+
+}
