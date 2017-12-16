e30e99c378187a519153b7766b9f6fde5e8fd921
VNiemi <ville.niemi@sykemediapaja.com>
2017-12-17 01:11:24 +0200

    Set banner to work with grid animation, ie grid animation starts after the banner is shown.

--- Deep Sea/Assets/Scripts/LevelBanner.cs
+++ Deep Sea/Assets/Scripts/LevelBanner.cs
@@ -38,6 +38,7 @@ public class LevelBanner : MonoBehaviour
         if (_position.x < -_moveLimits)
         {
             Destroy(gameObject);
+            Grid.StartEmphasis();
         }
     }
 
