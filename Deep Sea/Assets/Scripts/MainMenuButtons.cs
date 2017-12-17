c9aeaeb8ded2da75e8e4b718dd06fb5eca3c1766
VNiemi <ville.niemi@sykemediapaja.com>
2017-12-17 18:59:42 +0200

    Main menu animates. Buttons do nothing, but show up.

---
+++ Deep Sea/Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuButtons : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject _buttons;
+
+    private IEnumerator ShowMenu() {
+
+        yield return new WaitForSeconds(5.2f);
+        _buttons.SetActive(true);
+    }
+
+     
+
+    // Use this for initialization
+    void Start()
+    {        
+        StartCoroutine(ShowMenu());
+        _buttons.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Deep Sea/Assets/Scripts/MainMenuUP.cs
+++ Deep Sea/Assets/Scripts/MainMenuUP.cs
@@ -17,15 +17,33 @@ public class MainMenuUP : MonoBehaviour
 
     private float _speed;
 
+    private float _elapsedTime;
+
     // Use this for initialization
     void Start()
     {
+        // Set start position
         _position = gameObject.transform.localPosition;
+        _position.y = _startYPosition;
+        gameObject.transform.localPosition = _position;
+
+        _speed = (_endYPosition - _startYPosition) / _animationTime;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (_elapsedTime > _animationTime) return;
+
+        _position.y += _speed * Time.deltaTime;
+        _elapsedTime += Time.deltaTime;
+
+        if (_elapsedTime > _animationTime)
+        {
+            _position.y = _endYPosition;
+        }
+
+        gameObject.transform.localPosition = _position;
 
     }
 }
