b19d30a6824c91c114ae09290b3e1be55d176709
VNiemi <ville.niemi@sykemediapaja.com>
2017-12-17 12:47:51 +0200

    Changed canvas scaling mode of the UI to constant physical size which should remove the animation artefacts caused by scaling from reference to physical resolution. May cause issues in mobile. Also seems to contain lots of changes I do not remember making, but nothing seems broken?

---
+++ Deep Sea/Assets/Scripts/MainMenuUP.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuUP : MonoBehaviour
+{
+
+    // How long the main menu 'animations' last. Set in code as editing one file is actually simpler than using editor for multiple objects.
+    private float _animationTime = 5f;
+
+    [SerializeField]
+    private float _startYPosition;
+
+    [SerializeField]
+    private float _endYPosition;
+
+    private Vector3 _position;
+
+    private float _speed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
