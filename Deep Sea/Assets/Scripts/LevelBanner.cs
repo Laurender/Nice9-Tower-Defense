d01310c4221f027635d5d11a5813a28b95d4ff79
VNiemi <ville.niemi@cs.tamk.fi>
2018-01-23 14:38:08 +0200

    New level transition, first try, probably has to be changed. Boss music fix. Speed up effect fix.

--- Deep Sea/Assets/Scripts/LevelBanner.cs
+++ Deep Sea/Assets/Scripts/LevelBanner.cs
@@ -3,52 +3,17 @@ using System.Collections;
 
 public class LevelBanner : MonoBehaviour
 {
-
-    [SerializeField, Tooltip("The banner moves from x coordinate (this value) to -(this value).")]
-    private float _moveLimits;
-
-    [SerializeField]
-    private float _animationDuration;
-
-    private float _speed;
-
-    private Vector3 _position;
-
     [SerializeField]
     private Sprite[] _images;
+
+ 
 
     // Use this for initialization
     void Start()
     {
-        // Sets banner at starting position. Which should be outside the right edge.
-        _position = gameObject.transform.localPosition;
-        _position.x = _moveLimits;
-        gameObject.transform.localPosition = _position;
+        gameObject.GetComponent<UnityEngine.UI.Image>().sprite = _images[LevelManager.CurrentLevel];
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        // Move object left.
-        _position.x -= _speed *(Mathf.Abs(_position.x/_moveLimits)+.5f)* Time.deltaTime;
-        gameObject.transform.localPosition = _position;
-
-        // Until banner is outside left edge and can be destroyed.
-        if (_position.x < -_moveLimits)
-        {
-            Destroy(gameObject);
-            Grid.StartEmphasis();
-        }
-    }
-
-    public void Initialize(int level)
-    {
-        // Set up sprite renderer with correct graphics for the level.
-        gameObject.GetComponent<SpriteRenderer>().sprite = _images[level];
-
-        // Set movement speed;
-        _speed = 2 * _moveLimits / _animationDuration;
-
-    }
+  
 }
---
+++ Deep Sea/Assets/Scripts/Transition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition : MonoBehaviour {
+
+    [SerializeField, Tooltip("Game objects to disable when screen is touched.")]
+    private GameObject[] _partsOfTransition;
+
+    public void Close()
+    {
+        foreach(GameObject go in _partsOfTransition)
+        {
+            go.SetActive(false);
+        }
+    }
+}
