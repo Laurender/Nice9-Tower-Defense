c8a7cc2b3cf5ee243825b4a97d7516d9c2db35cc
VNiemi <ville.niemi@cs.tamk.fi>
2018-02-01 16:33:33 +0200

    HelpBanner

---
+++ Deep Sea/Assets/Scripts/HelpBanners.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpBanners : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _buildBanner;
+
+    [SerializeField]
+    private GameObject _wavesBanner;
+
+
+    private static HelpBanners _instance;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public static void ShowBuildBanner()
+    {
+        _instance.StartCoroutine(_instance.Banner(_instance._buildBanner));
+    }
+
+    public static void ShowWavesBanner()
+    {
+        _instance.StartCoroutine(_instance.Banner(_instance._wavesBanner));
+    }
+
+    public IEnumerator Banner(GameObject banner)
+    {
+        banner.SetActive(true);
+
+        for (float y = -100; y < 0; y += 5)
+        {
+            yield return new WaitForSecondsRealtime(.05f);
+        }
+
+        for (float y = 0; y > -100; y -= 5)
+        {
+            yield return new WaitForSecondsRealtime(.05f);
+        }
+        banner.SetActive(false);
+    }
+}
