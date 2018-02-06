cd9e071de42b45c1358f26378f909968ecf71316
VNiemi <ville.niemi@cs.tamk.fi>
2018-02-06 12:12:55 +0200

    The GridUi no longer works if transition is open. So there should not be build menu open when you start the level.

--- Deep Sea/Assets/Scripts/Transition.cs
+++ Deep Sea/Assets/Scripts/Transition.cs
@@ -7,6 +7,7 @@ public class Transition : MonoBehaviour {
     [SerializeField, Tooltip("Game objects to disable when screen is touched.")]
     private GameObject[] _partsOfTransition;
 
+    private static bool _closed;
 
     public void Awake()
     {
@@ -14,17 +15,40 @@ public class Transition : MonoBehaviour {
         {
             go.SetActive(true);
         }
+
+        _closed = false;
+
     }
 
+    public static bool IsClosed
+    {
+        get { return _closed; }
+       
+    }
+    
 
     public void Close()
     {
+
+        StartCoroutine(CloseThis());
+
+        
+    }
+
+    // Is actually closed on the mouse up AFTER the tap to avoid activating a menu.
+    private IEnumerator CloseThis()
+    {
+        yield return new WaitUntil(() => Input.GetMouseButtonUp(0));
+        yield return new WaitForFixedUpdate();
+
         HelpBanners.ShowBuildBanner();
-        foreach(GameObject go in _partsOfTransition)
+        foreach (GameObject go in _partsOfTransition)
         {
 
             go.SetActive(false);
         }
+
+        _closed = true;
     }
 
  
